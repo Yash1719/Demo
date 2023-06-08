50cbd195be5d690157794c9dc86507085f5f88a9
Yash1719 <76112649+Yash1719@users.noreply.github.com>
2023-06-08 16:55:13 +0530

    first commit

---
+++ queue.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node* rear;
+    struct node* front;
+};
+
+
+
+
+int main()
+{
+    int choice;
+    int flag=1;
+    while(flag)
+    {
+        
+    }
+}
